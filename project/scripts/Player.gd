ca05994bdff83f730bd7a18cc4614a5fed589fa1
lukyxu <luky.xu@gmail.com>
2019-01-26 19:38:50 +0000

    implemented movement

--- project/scripts/Player.gd
+++ project/scripts/Player.gd
@@ -6,24 +6,56 @@ var damage = 0
 onready var disabled_timer = get_node("Disabled")
 
 export var walk_speed = 0
-export var dash_speed = 0
 
 var up_down = false
 var down_down = false
 var left_down = false
 var right_down = false
+var shift_down = false
+var hit = false
+var velocity
 
+func get_input():
+	up_down = false
+	down_down = false
+	left_down = false
+	right_down = false
+	right_down = false
+	shift_down = false
+	if Input.is_action_pressed("ui_left"):
+		left_down = true
+	if Input.is_action_pressed("ui_right"):
+		right_down = true
+	if Input.is_action_pressed("ui_up"):
+		up_down = true
+	if Input.is_action_pressed("ui_down"):
+		down_down = true
+	if Input.is_action_pressed("ui_tempshift"):
+		shift_down = true
+	
+
+func get_normalized_velocity():
+	get_input()
+	velocity = Vector2()
+	if disabled_timer.is_stopped():
+		if up_down:
+			velocity.y -= 1
+		if down_down:
+			velocity.y += 1
+		if left_down:
+			velocity.x -= 1
+		if right_down:
+			print("rightdown")
+			velocity.x += 1
+	return velocity.normalized()
+
+#implement movement
+
+#implement what happens after you get hit
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
-
-func _physics_process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-
-	
 	pass
 
 func damage(amount):
