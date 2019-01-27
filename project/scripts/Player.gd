260ad1be0b153939678beef2a0f9ff2cab48fbd8
Seiya Aoki <aokiseiya1@gmail.com>
2019-01-27 07:58:17 +0000

    implemented_attacks

--- project/scripts/Player.gd
+++ project/scripts/Player.gd
@@ -2,16 +2,16 @@ extends KinematicBody2D
 
 # class member variables go here, for example:
 var damage = 0
-var disabled_timer
+onready var disabled_timer = get_node("Disabled")
 var invul_timer
 var lives = 0
 var spawn_pos = Vector2(0,0)
+var knockback_remaining = 0
 
 var dir_facing = Vector2(1,0)
 
 func _ready():
-	disabled_timer = get_node("Disabled")
-	disabled_timer.set_wait_time(1)
+	pass
 
 export var walk_speed = 0
 
@@ -25,31 +25,13 @@ var attack_down = false
 var attack_damage = 10
 var velocity
 
+var disabled = false
 var invulnerable = false
-
-#func get_input():
-#	up_down = false
-#	down_down = false
-#	left_down = false
-#	right_down = false
-#	shift_down = false
-#	if Input.is_action_pressed("ui_left"):
-#		left_down = true
-#	if Input.is_action_pressed("ui_right"):
-#		right_down = true
-#	if Input.is_action_pressed("ui_up"):
-#		up_down = true
-#	if Input.is_action_pressed("ui_down"):
-#		down_down = true
-#	if Input.is_action_just_pressed("ui_tempshift"):
-#		shift_down = true
-#		print("down")
-	
 
 func get_normalized_velocity():
 #	get_input()
 	velocity = Vector2()
-	if disabled_timer.is_stopped():
+	if !disabled:
 		if up_down:
 			velocity.y -= 1
 		if down_down:
@@ -64,6 +46,11 @@ func get_normalized_velocity():
 func damage(amount):
 	damage += amount
 
+func disable_for(amount):
+	disabled_timer.wait_time = amount
+	disabled_timer.start()
+	disabled = true
+
 func is_self_box(area):
 	for box in get_node("CollisionBoxes").get_children():
 		if area == box:
@@ -77,10 +64,11 @@ func is_hit_box(area):
 	return false
 
 
-func _on_HitBox_area_entered(area):
-	if area.is_in_group("attacks") && !is_self_box(area):
-		#$area.disabled = false
-		print(area.dir)
+#func _on_HitBox_area_entered(area):
+#	if area.is_in_group("attacks") && !is_self_box(area):
+#		damage(area.damage)
+#		print(area.knockback)
+#		knockedback(area.knockback, area.dir)
 
 func make_invulnerable(time):
 	invul_timer = get_node("Invul")
@@ -91,7 +79,6 @@ func make_invulnerable(time):
 func init():
 	damage = 0
 	dir_facing = Vector2(1,0)
-	position = Vector2(400,300) 
 	
 
 func respawn():
@@ -103,5 +90,14 @@ func respawn():
 func _on_Invul_timeout():
 	invulnerable = false
 	invul_timer.stop()
-	
 
+func knockedback(amount, dir):
+	velocity = dir * GAME_CONFIG.knockback_speed
+	knockback_remaining = amount
+	print(knockback_remaining)
+
+func _on_Disabled_timeout():
+	print("stopped")
+	disabled = false
+	disabled_timer.stop()
+	
